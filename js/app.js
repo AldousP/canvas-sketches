c8f3d85022e17fababb98a074da36fdac7d760ca
AldousP <aldouspryce@gmail.com>
2017-07-07 19:29:17 -0400

    Scope app correctly.

--- js/app.js
+++ js/app.js
@@ -1,74 +1,43 @@
-"use strict";
+(function () {
+  "use strict";
+  var fullscreenButtons = document.getElementsByClassName("fullscreen-toggle");
+  var listItemTemplate = document.getElementById("list-item-template");
+  var entityListItemTemplate = document.getElementById("entity-list-item-template");
+  var appTitle = document.getElementById("app-title");
 
-var systemList = document.getElementById("systems-list");
-var entityList = document.getElementById("entities-list");
-
-var fullscreenButtons = document.getElementsByClassName("fullscreen-toggle");
-var listItemTemplate = document.getElementById("list-item-template");
-var entityListItemTemplate = document.getElementById("entity-list-item-template");
-
-var appTitle = document.getElementById("app-title");
-
-attachHandlers();
-
-/* Render the UI lists in the DOM */
-function renderLists() {
-  systemList.innerHTML = '';
-  entityList.innerHTML = '';
-  systemHandler.systems.forEach(function (system) {
-    var newEntry = listItemTemplate.cloneNode(true);
-    newEntry.removeAttribute("id");
-    newEntry.childNodes[1].innerHTML = system.name;
-    var button = newEntry.getElementsByTagName('button')[0];
-    button.setAttribute("onclick", "javascript: pauseSystem('" + system.ID + "');");
-    if (system.paused) {
-      newEntry.getElementsByTagName('button')[0].classList.add('active');
-    }
-    systemList.appendChild(newEntry);
-  });
-
-  var keys = Object.keys(entityHandler.entities);
-  keys.forEach(function (key) {
-    var entity = entityHandler.entities[key];
-    var newEntry = entityListItemTemplate.cloneNode(true);
-    newEntry.removeAttribute("id");
-    newEntry.childNodes[1].innerHTML = entity.name ? entity.name : entity.ID;
-    var button = newEntry.getElementsByTagName('button')[0];
-    button.setAttribute("onclick", "javascript: logEntity('" + entity.ID + "');");
-    entityList.appendChild(newEntry);
-  });
-}
-
-function attachHandlers() {
-  for (var i = 0; i < fullscreenButtons.length; i++) {
-    fullscreenButtons[i].onclick = function (e) {
-      document.body.classList.toggle("full-screen");
-      for (var j = 0; j < fullscreenButtons.length; j++) {
-        fullscreenButtons[j].classList.add("active");
-      }
-      window.setTimeout(function () {
-        for (var i = 0; i < fullscreenButtons.length; i++) {
-          fullscreenButtons[i].classList.remove("active");
+  attachHandlers();
+  function attachHandlers() {
+    for (var i = 0; i < fullscreenButtons.length; i++) {
+      fullscreenButtons[i].onclick = function (e) {
+        document.body.classList.toggle("full-screen");
+        for (var j = 0; j < fullscreenButtons.length; j++) {
+          fullscreenButtons[j].classList.add("active");
         }
-      }, 100);
+        window.setTimeout(function () {
+          for (var i = 0; i < fullscreenButtons.length; i++) {
+            fullscreenButtons[i].classList.remove("active");
+          }
+        }, 100);
+      }
     }
   }
-}
 
-document.body.addEventListener("smProgramLoaded", function (event) {
-  appTitle.innerText = event.detail.programName;
-});
+  document.body.addEventListener("smProgramLoaded", function (event) {
+    appTitle.innerText = event.detail.programName;
+  });
 
-document.body.addEventListener("smProgramUnloaded", function (event) {
-  appTitle.innerText = "No Program Loaded";
-});
+  document.body.addEventListener("smProgramUnloaded", function (event) {
+    appTitle.innerText = "No Program Loaded";
+  });
 
-function loadMountExample() {
-  sm.loadProgram(new MountExample());
-}
+  function loadMountExample() {
+    sm.loadProgram(new MountExample());
+  }
 
-function loadCameraTests() {
-  sm.loadProgram(new CameraTests());
-}
+  function loadCameraTests() {
+    sm.loadProgram(new CameraTests());
+  }
 
-sm.init("canvas", new CameraTests());
+  sm.init("canvas", new CameraTests());
+}());
+

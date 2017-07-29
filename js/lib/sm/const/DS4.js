40b6080c5f2d1599e0a40c1e8ff31f45a089dd96
AldousP <aldouspryce@gmail.com>
2017-07-28 23:04:47 -0400

    Add DS4 mappings.

---
+++ js/lib/sm/const/DS4.js
@@ -0,0 +1,19 @@
+DS4 = {
+  up: 12,
+  left: 14,
+  right: 15,
+  down: 13,
+  triangle: 3,
+  square: 2,
+  cross: 0,
+  circle: 1,
+  L1: 4,
+  L2: 6,
+  R1: 5,
+  R2: 7,
+  leftstick: 10,
+  rightstick: 11,
+  share: 8,
+  options: 9,
+  touchpad: 17
+};
